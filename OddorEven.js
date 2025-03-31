0677ec89236d9d9d807ac3442b099676b7723f8f
Suganya-Gopinathan <suganyagopinathan1994@gmail.com>
2025-03-31 19:00:49 +0530

    Day 2 Session Assignment Completion

---
+++ OddorEven.js
@@ -0,0 +1,11 @@
+function isOddOrEven(number)
+{
+    if(number%2===0){
+        console.log("The Number is Even");
+    }
+    else{
+        console.log("The Number is Odd");
+    }
+}
+
+isOddOrEven(10);
